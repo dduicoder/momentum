6f629ff9e52e96448a6064b4bda7c9945f79f339
dduicoder <sijinddui2@gmail.com>
2021-12-18 21:08:27 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 const images = ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-document.body.classList.add("bg");
-document.body.classList.add(chosenImage);
+const main = document.querySelector(".background");
+main.classList.add("bg");
+main.classList.add(chosenImage);
