a8fbbea1167b6ab89cd42d48e66ecbbfc258ec4d
dduicoder <sijinddui2@gmail.com>
2022-06-17 17:00:43 +0900

    fin

--- js/greetings.js
+++ js/greetings.js
@@ -6,42 +6,42 @@ const greet = document.querySelector(".greet");
 const blur = document.querySelector(".blur");
 
 const colors = [
-    "#ff1acd",
-    "#ff1acd",
-    "#ff1acd",
-    "#1fe98b",
-    "#33ee97",
-    "#f53b57",
-    "#f53b57",
-    "#f53b57",
-    "#e2354f",
-    "#ff5e57"
-  ];
-  
+  "#ff1acd",
+  "#ff1acd",
+  "#ff1acd",
+  "#1fe98b",
+  "#33ee97",
+  "#f53b57",
+  "#f53b57",
+  "#f53b57",
+  "#e2354f",
+  "#ff5e57",
+];
+
 const color1 = colors[Math.floor(Math.random() * colors.length)];
 greet.style.background = `linear-gradient(45deg, ${color1}, #ffc900)`;
 
 function loginSubmit(event) {
-    event.preventDefault();
-    const username = loginInput.value;
+  event.preventDefault();
+  const username = loginInput.value;
 
-    localStorage.setItem("username", username);
-    greeting.innerText = `Hello ${username}`;
+  localStorage.setItem("username", username);
+  greeting.innerText = `Hello ${username}`;
 
-    blur.classList.add("blur-off");
-    loginForm.classList.add("disappear");
-    setTimeout(function() { 
-        loginDiv.remove(); 
-        blur.remove();
-    }, 1500);
+  blur.classList.add("blur-off");
+  loginForm.classList.add("disappear");
+  setTimeout(function () {
+    loginDiv.remove();
+    blur.remove();
+  }, 1500);
 }
 
 const savedUserName = localStorage.getItem("username");
 
-if(savedUserName === null) {
-    loginForm.addEventListener("submit", loginSubmit);
+if (savedUserName === null) {
+  loginForm.addEventListener("submit", loginSubmit);
 } else {
-    loginDiv.remove();
-    blur.remove();
-    greeting.innerText = `Hello ${savedUserName}`;
+  loginDiv.remove();
+  blur.remove();
+  greeting.innerText = `Hello ${savedUserName}`;
 }
